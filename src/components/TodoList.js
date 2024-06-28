aa31a44d324575ca9ba67453c8e8161291c2e39b
RiddickAngiela <angielariddick@gmail.com>
2024-06-28 10:54:39 +0300

    adding classes names

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,7 @@
 // src/components/TodoList.js
 import React, { useState, useEffect } from "react";
+import "./TodoList.css";
+
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
@@ -39,26 +41,31 @@ const TodoList = () => {
   };
 
   return (
-    <div>
-      <h1>Riddick To-Do List</h1>
+    <div className="todo-container">
+      <h1 className="todo-title">Riddick To-Do List</h1>
       <input
         type="text"
+        className="todo-input"
         value={newTodo}
         onChange={(e) => setNewTodo(e.target.value)}
         placeholder="Add a new to-do"
       />
-      <button onClick={addTodo}>Add</button>
-      <ul>
+      <button className="todo-button" onClick={addTodo}>
+        Add
+      </button>
+      <ul className="todo-list">
         {todos.map((todo) => (
           <li
             key={todo.id}
-            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+            className={`todo-item ${todo.completed ? "completed" : ""}`}
           >
             {todo.title} - {todo.timestamp}
-            <button onClick={() => toggleComplete(todo.id)}>
-              {todo.completed ? "Undo" : "Complete"}
-            </button>
-            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            <div className="todo-item-buttons">
+              <button onClick={() => toggleComplete(todo.id)}>
+                {todo.completed ? "Undo" : "Complete"}
+              </button>
+              <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            </div>
           </li>
         ))}
       </ul>
